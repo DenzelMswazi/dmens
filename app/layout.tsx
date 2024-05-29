0bf073d9c8ce2e3ba13d112c9220e0a8ce60ed90
deeMswazi <95373816+deemswazi@users.noreply.github.com>
2024-05-29 08:26:23 +0300

    import footer

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Poppins } from "next/font/google";
 import "./globals.css";
 import Navbar from "./components/nav/Navbar";
+import Footer from "./components/footer/Footer";
 
 const poppins = Poppins({ subsets: ["latin"], weight: ["400", "700"] });
 
@@ -22,6 +23,7 @@ export default function RootLayout({
         <main>
         {children}
         </main>
+        <Footer />
         </body>
     </html>
   );
