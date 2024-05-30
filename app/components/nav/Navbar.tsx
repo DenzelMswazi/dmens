245c9bcdd6230f344908d2b7844fc76bc6ea67d4
deeMswazi <95373816+deemswazi@users.noreply.github.com>
2024-05-30 10:01:02 +0300

    navbar classes

--- app/components/nav/Navbar.tsx
+++ app/components/nav/Navbar.tsx
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import Container from "../Container";
 
 const Navbar = () => {
@@ -5,7 +6,14 @@ const Navbar = () => {
       <div className="sticky top-0 w-full bg-slate-200 z-30 shadow-sm">
         <div className="py-4 border-b-[1px]">
           <Container>
-            
+            <div className="flex items-center justify-between gap-3 md-gap-0">
+              <Link href={"/"}>DMENS GIFT SHOP</Link>
+              <div>Search Products</div>
+              <div>
+                <div>CartCount</div>
+                <div>UserMenu</div>
+              </div>
+            </div>
           </Container>
         </div>
       </div>
