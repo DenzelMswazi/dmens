b2365b8bb7231664cbb92a964f3a05fdc8883cf5
deeMswazi <95373816+deemswazi@users.noreply.github.com>
2024-05-30 15:27:39 +0300

    chore(fixes)

--- app/components/footer/Footer.tsx
+++ app/components/footer/Footer.tsx
@@ -4,12 +4,17 @@ import FooterList from "./FooterList";
 
 const Footer = () => {
     return (
-      <footer className="bg-slate-700 text-slate-200 text-sm mt-16">
+      <footer className="bg-black text-white text-sm mt-16">
         <Container>
           <div className="flex flex-col md:flex-row justify-between pt-16 pb-8">
             <FooterList>
-              <h3>Shop Categories</h3>
-              <Link></Link>
+              <h3>Team Categories</h3>
+              <Link href={"#"}>MUFC</Link>
+              <Link href={"#"}>Arsenal</Link>
+              <Link href={"#"}>Liverpool</Link>
+              <Link href={"#"}>Manchester city</Link>
+              <Link href={"#"}>Chelsea</Link>
+              <Link href={"#"}>Real madrid</Link>
             </FooterList>
           </div>
         </Container>
--- app/components/nav/Navbar.tsx
+++ app/components/nav/Navbar.tsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 import Container from "../Container";
 import { Plus_Jakarta_Sans } from "next/font/google";
 
-const plus_Jakarta_Sans = Plus_Jakarta_Sans({ subsets: ["latin"], weight: ["400"]});
+const plus_Jakarta_Sans = Plus_Jakarta_Sans({ subsets: ["latin"], weight: ["600"]});
 
 const Navbar = () => {
     return (
@@ -10,7 +10,7 @@ const Navbar = () => {
         <div className="py-4 border-b-[1px]">
           <Container>
             <div className="flex items-center justify-between gap-3 md:gap-0">
-              <Link href={"/"} className={`${plus_Jakarta_Sans.className} font-bold text-2xl`}>DMENS GIFT SHOP</Link>
+              <Link href={"/"} className={`${plus_Jakarta_Sans.className} font-extrabold text-2xl`}>DMENS GIFT SHOP</Link>
               <div className="hidden md:block">Search Products</div>
               <div className="flex items-center gap-8 md:gap-12">
                 <div>CartCount</div>
