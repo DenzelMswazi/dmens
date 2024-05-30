36593102a712dcbd432e8263d1757a6b741bc2e8
deeMswazi <95373816+deemswazi@users.noreply.github.com>
2024-05-30 10:13:39 +0300

    add fonts

--- app/components/nav/Navbar.tsx
+++ app/components/nav/Navbar.tsx
@@ -1,15 +1,18 @@
 import Link from "next/link";
 import Container from "../Container";
+import { Plus_Jakarta_Sans } from "next/font/google";
+
+const plus_Jakarta_Sans = Plus_Jakarta_Sans({ subsets: ["latin"], weight: ["400"]})
 
 const Navbar = () => {
     return (
       <div className="sticky top-0 w-full bg-slate-200 z-30 shadow-sm">
         <div className="py-4 border-b-[1px]">
           <Container>
-            <div className="flex items-center justify-between gap-3 md-gap-0">
+            <div className="flex items-center justify-between gap-3 md:gap-0">
               <Link href={"/"}>DMENS GIFT SHOP</Link>
-              <div>Search Products</div>
-              <div>
+              <div className="hidden md:block">Search Products</div>
+              <div className="flex items-center gap-8 md:gap-12">
                 <div>CartCount</div>
                 <div>UserMenu</div>
               </div>
