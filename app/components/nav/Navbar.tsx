a27034d35ae06c348a55753ddb3afe32805a5389
deeMswazi <95373816+deemswazi@users.noreply.github.com>
2024-05-30 09:49:44 +0300

    container import

--- app/components/nav/Navbar.tsx
+++ app/components/nav/Navbar.tsx
@@ -1,6 +1,14 @@
+import Container from "../Container";
+
 const Navbar = () => {
     return (
-      <div>Navbar</div>
+      <div className="sticky top-0 w-full bg-slate-200 z-30 shadow-sm">
+        <div className="py-4 border-b-[1px]">
+          <Container>
+            
+          </Container>
+        </div>
+      </div>
     );
 }
 export default Navbar;
