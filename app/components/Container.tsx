1bd187cbed98cb2c2e7f138f600cdd55681cefa9
deeMswazi <95373816+deemswazi@users.noreply.github.com>
2024-05-30 09:39:31 +0300

    container classes

--- app/components/Container.tsx
+++ app/components/Container.tsx
@@ -4,7 +4,7 @@ interface ContainerProps {
 
 const Container: React.FC<ContainerProps> = ({children}) => {
   return (
-    <div>{children}</div>
+    <div className="max-w-[1920px] mx-auto xl:px-20 md:px-2 px-4">{children}</div>
   )
 }
 
