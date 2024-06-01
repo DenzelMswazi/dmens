c3042da0fd6bdaab19e2670cf5d241b7ba91ae52
deeMswazi <95373816+deemswazi@users.noreply.github.com>
2024-06-01 20:37:18 +0300

    products details

---
+++ utils/products.tsx
@@ -0,0 +1,28 @@
+//I am changing the image links from firebase to amazon, firebase has issues with these direct links. Please remember to add the amazon link(that will show up at the error) instead of firebase at the cofig for it to work out.
+
+export const products = [
+    {
+      id: "64a654593e91b8e73a351e9b",
+      name: "Manchester United 2024/25 Home Jersey",
+      description: "We get the first detailed look at the authentic version of the Manchester United 24-25 home kit today. It is made by Adidas and will be worn in the 2024-2025 Premier League.",
+      price: 2000,
+      brand: "Adidas",
+      category: "Jersey",
+      inStock: true,
+      images: [
+        {
+          color: "Red",
+          colorCode: "#FF0000",
+          image:
+            "/Manchester_United_24-25_Home.jpg",
+        },
+        {
+          color: "Red",
+          colorCode: "#FF0000",
+          image:
+            "Manchester_United_24-25_Home_Kit.jpg",
+        },
+      ],
+      reviews: [],
+    },
+];
