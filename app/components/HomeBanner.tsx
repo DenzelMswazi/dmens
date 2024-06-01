c4d3f65097166d06e140e8b96233360d8149fdae
deeMswazi <95373816+deemswazi@users.noreply.github.com>
2024-06-01 18:40:05 +0300

    chore:utils

--- app/components/HomeBanner.tsx
+++ app/components/HomeBanner.tsx
@@ -6,7 +6,7 @@ const HomeBanner = () => {
           <div className="mx-auto px-8 py-12 flex flex-col gap-2 md:flex-row items-center justify-evenly">
             <div className="mb-8 md:mb-0 text-center">
                 <h1 className="text-4xl md:text-6xl font-bold text-white mb-4">Game Ready!!</h1>
-                <p className="text-lg md:text-xl text-black mb-2">Topnotch quality for best prices</p>
+                <p className="text-lg md:text-xl text-black mb-2">Top - Notch quality for best prices</p>
                 <p className="text-2xl md:text-5xl text-yellow-500 font-bold">GET A FREE BADGE</p>
             </div>
             <div className="w-1/3 relative hidden md:block" style={{ aspectRatio: '16/9'}}>
