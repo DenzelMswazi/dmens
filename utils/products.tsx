a70b9e815d470f685a6b1aeaad3a4e90075f577e
deeMswazi <95373816+deemswazi@users.noreply.github.com>
2024-06-01 22:37:06 +0300

    truncateText function

--- utils/products.tsx
+++ utils/products.tsx
@@ -1,5 +1,8 @@
 //I am changing the image links from firebase to amazon, firebase has issues with these direct links. Please remember to add the amazon link(that will show up at the error) instead of firebase at the cofig for it to work out.
 
+import { ImageResponse } from "next/server";
+import { MdDescription } from "react-icons/md";
+
 export const products = [
     {
       id: "64a654593e91b8e73a351e9b",
@@ -20,9 +23,27 @@ export const products = [
           color: "Red",
           colorCode: "#FF0000",
           image:
-            "Manchester_United_24-25_Home_Kit.jpg",
+            "/Manchester_United_24-25_Home_Kit.jpg",
         },
       ],
       reviews: [],
+    },
+    {
+        id: "64a4ebe300900d44bb50628a",
+        name: "Arsenal 2024/25 Home Jersey",
+        description: "",
+        price: 2000,
+        brand: "Adidas",
+        category: "Jersey",
+        inStock: true,
+        images: [
+            {
+            color: "Red",
+            colorCode: "#FF0000",
+            image:
+                 "/Arsenal.jpg",
+            },
+        ],
+        reviews: [],
     },
 ];
