1ac80d3915e88c62368cfdad70009776e426a36e
deeMswazi <95373816+deemswazi@users.noreply.github.com>
2024-06-02 19:39:11 +0300

    addedc image data

--- app/components/products/ProductCard.tsx
+++ app/components/products/ProductCard.tsx
@@ -1,5 +1,7 @@
 'use client';
 
+import Image from "next/image";
+
 interface ProductCardProps {
     data: any
 
@@ -9,8 +11,12 @@ const ProductCard:React.FC<ProductCardProps> = ({data}) => {
     return (
         <div className="col-span-1 cursor-pointer border-[1.2px] border-slate-200 bg-slate-50 rounded-sm p-2 transition hover:scale-105 text-center text-sm">
           <div className="flex flex-col items-center w-full gap-1">
-           <div>
-
+           <div className="aspect-square overflow-hidden relative w-full">
+              <Image 
+              src={data.images[0].image}
+              fill
+              alt={data.name}
+              className="w-full h-full object-contain"/>
            </div>
            <div>
 
