846fa3d2956bbe200a8275538fe2e9c54afd076a
deeMswazi <95373816+deemswazi@users.noreply.github.com>
2024-06-02 19:33:29 +0300

    define product props

--- app/components/products/ProductCard.tsx
+++ app/components/products/ProductCard.tsx
@@ -1,6 +1,11 @@
 'use client';
 
-const ProductCard = ({data}) => {
+interface ProductCardProps {
+    data: any
+
+}
+
+const ProductCard:React.FC<ProductCardProps> = ({data}) => {
     return (
         <div className="col-span-1 cursor-pointer border-[1.2px] border-slate-200 bg-slate-50 rounded-sm p-2 transition hover:scale-105 text-center text-sm">
           <div className="flex flex-col items-center w-full gap-1">
