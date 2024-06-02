cb126523ce47b0f5c70eafcb5bf7959b57148604
deeMswazi <95373816+deemswazi@users.noreply.github.com>
2024-06-02 19:29:42 +0300

    width size

--- app/components/products/ProductCard.tsx
+++ app/components/products/ProductCard.tsx
@@ -3,7 +3,7 @@
 const ProductCard = () => {
     return (
         <div className="col-span-1 cursor-pointer border-[1.2px] border-slate-200 bg-slate-50 rounded-sm p-2 transition hover:scale-105 text-center text-sm">
-          <div>
+          <div className="flex flex-col items-center w-full gap-1">
            <div>
 
            </div>
