c14fe7c5e30b9d10f1d98fdd3eaf543f14252699
deeMswazi <95373816+deemswazi@users.noreply.github.com>
2024-06-02 19:30:56 +0300

    structure to recieve data

--- app/components/products/ProductCard.tsx
+++ app/components/products/ProductCard.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-const ProductCard = () => {
+const ProductCard = ({data}) => {
     return (
         <div className="col-span-1 cursor-pointer border-[1.2px] border-slate-200 bg-slate-50 rounded-sm p-2 transition hover:scale-105 text-center text-sm">
           <div className="flex flex-col items-center w-full gap-1">
