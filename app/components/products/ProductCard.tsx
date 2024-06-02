75b3ce2dd3ae4e5a7026ca5a73b7b7aa26d96852
deeMswazi <95373816+deemswazi@users.noreply.github.com>
2024-06-02 19:27:18 +0300

    Add parent div classes

--- app/components/products/ProductCard.tsx
+++ app/components/products/ProductCard.tsx
@@ -2,7 +2,7 @@
 
 const ProductCard = () => {
     return (
-        <div>
+        <div className="col-span-1 cursor-pointer border-[1.2px] border-slate-200 bg-slate-50 rounded-sm p-2 transition hover:scale-105 text-center text-sm">
           <div>
            <div>
 
