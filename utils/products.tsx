c5044ec43e112e96f681dc0140575f24430b7cd4
deeMswazi <95373816+deemswazi@users.noreply.github.com>
2024-06-03 22:23:14 +0300

    added user review

--- utils/products.tsx
+++ utils/products.tsx
@@ -1,6 +1,7 @@
 //I am changing the image links from firebase to amazon, firebase has issues with these direct links. Please remember to add the amazon link(that will show up at the error) instead of firebase at the cofig for it to work out.
 
 import { ImageResponse } from "next/server";
+import { comment } from "postcss";
 import { GiBrandyBottle } from "react-icons/gi";
 import { MdDescription } from "react-icons/md";
 
@@ -66,7 +67,7 @@ export const products = [
         reviews: [],
     },
     {
-        id: "",
+        id: "64a4ebe300900d44bb50628a",
         name: "Liverpool 2024-25 Home Jersey",
         description: " Inspired by Rome 84, Nike officially launched the Liverpool 24-25 home shirt with a unique YNWA pattern.",
         price: 2000,
@@ -81,6 +82,27 @@ export const products = [
               "/liverpool-home.jpg"
             },
         ],
-        reviews: [],
+        reviews: [
+          {
+            id: "64a65a6158b470c6e06959ee",
+            userId: "6475af156bad4917456e6e1e",
+            productId: "64a4ebe300900d44bb50628a",
+            rating: 5,
+            comment: "I love the new jersey, Great quality and fits perfectly.",
+            createdDate: "2024-09-01T00:00:00Z",
+            user: {
+              id: "6475af156bad4917456e6e1e",
+              name: "Denzel Mswazi",
+              email: "example@gmail.com",
+              emailVerified: true,
+              image: "",
+              hashedPassword: "null",
+              createdAt: "2024-09-01T00:00:00Z",
+              updatedAt: "2024-09-01T00:00:00Z",
+              role: "user",
+            },
+
+          },
+        ],
     },
 ];
