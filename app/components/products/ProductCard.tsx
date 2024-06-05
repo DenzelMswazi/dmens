a0263e09ca38e74a694505d6b8e1d17adc680113
deeMswazi <95373816+deemswazi@users.noreply.github.com>
2024-06-05 13:09:23 +0300

    add product type data

--- app/components/products/ProductCard.tsx
+++ app/components/products/ProductCard.tsx
@@ -12,7 +12,7 @@ interface ProductCardProps {
 
 const ProductCard:React.FC<ProductCardProps> = ({data}) => {
 
-    const productRating = data.reviews.reduce((acc, item) => item.rating + acc, 0) / data.reviews.length;
+    const productRating = data.reviews.reduce((acc:number, item:any) => item.rating + acc, 0) / data.reviews.length;
 
     return (
         <div className="col-span-1 cursor-pointer border-[1.2px] border-slate-200 bg-slate-50 rounded-sm p-2 transition hover:scale-105 text-center text-sm">
