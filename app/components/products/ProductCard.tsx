ccad627fde56497e7b4423ff2ac5d5f25949fbcf
deeMswazi <95373816+deemswazi@users.noreply.github.com>
2024-06-05 12:50:03 +0300

    product rating accumulator

--- app/components/products/ProductCard.tsx
+++ app/components/products/ProductCard.tsx
@@ -11,6 +11,9 @@ interface ProductCardProps {
 }
 
 const ProductCard:React.FC<ProductCardProps> = ({data}) => {
+
+    const productRating = data.reviews.reduce((acc, item) => item.rating + acc, 0) / data.reviews.length;
+
     return (
         <div className="col-span-1 cursor-pointer border-[1.2px] border-slate-200 bg-slate-50 rounded-sm p-2 transition hover:scale-105 text-center text-sm">
           <div className="flex flex-col items-center w-full gap-1">
@@ -25,7 +28,7 @@ const ProductCard:React.FC<ProductCardProps> = ({data}) => {
               {truncateText(data.name)}
            </div>
             <div>
-                <Rating value={5} readOnly/>
+                <Rating value={4} readOnly/>
             </div>
             <div>{data.reviews.length} reviews</div>
             <div className="font-semibold">{formatPrice(data.price)}</div>
