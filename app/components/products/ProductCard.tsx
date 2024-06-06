4889460842648adc449a66b6d8351b68e4df00a7
deeMswazi <95373816+deemswazi@users.noreply.github.com>
2024-06-06 10:20:25 +0300

    import useRouter from next navigation

--- app/components/products/ProductCard.tsx
+++ app/components/products/ProductCard.tsx
@@ -4,6 +4,7 @@ import { formatPrice } from "@/utils/formatPrice";
 import { truncateText } from "@/utils/truncateText";
 import { Rating } from "@mui/material";
 import Image from "next/image";
+import { useRouter } from "next/navigation";
 
 interface ProductCardProps {
     data: any
@@ -11,6 +12,8 @@ interface ProductCardProps {
 }
 
 const ProductCard:React.FC<ProductCardProps> = ({data}) => {
+
+    const router = useRouter();
 
     const productRating = data.reviews.reduce((acc:number, item:any) => item.rating + acc, 0) / data.reviews.length;
 
